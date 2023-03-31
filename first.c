c98c8b2f0370e5bbfb85f417fe9b680e0f718ffe
Asif Mahmud <asifmahmud1133@gmail.com>
2023-03-31 23:38:34 +0600

    My First Code

---
+++ first.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+int main()
+{
+    printf("I love Muhammad SAW\n");
+    printf("I love My Mother, Murshid after Prophet\n");
+    printf("My Third Love is my wife and Child");
+    return 0;
+}
